d04d5236ae45b3b695723f557f8510f0c8374553
Kevin <102828189+Bleak9@users.noreply.github.com>
2022-06-30 01:09:51 -0400

    Add files via upload

---
+++ target/generated-sources/antlr4/ParserTBaseListener.java
@@ -0,0 +1,387 @@
+// Generated from ParserT.g4 by ANTLR 4.4
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link ParserTListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class ParserTBaseListener implements ParserTListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSuma(@NotNull ParserTParser.SumaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSuma(@NotNull ParserTParser.SumaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAsignacion(@NotNull ParserTParser.AsignacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAsignacion(@NotNull ParserTParser.AsignacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterReceta(@NotNull ParserTParser.RecetaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitReceta(@NotNull ParserTParser.RecetaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSenlogica(@NotNull ParserTParser.SenlogicaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSenlogica(@NotNull ParserTParser.SenlogicaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCos(@NotNull ParserTParser.CosContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCos(@NotNull ParserTParser.CosContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclavar(@NotNull ParserTParser.DeclavarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclavar(@NotNull ParserTParser.DeclavarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEntero(@NotNull ParserTParser.EnteroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEntero(@NotNull ParserTParser.EnteroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(@NotNull ParserTParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(@NotNull ParserTParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperacion(@NotNull ParserTParser.OperacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperacion(@NotNull ParserTParser.OperacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterImpresion(@NotNull ParserTParser.ImpresionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitImpresion(@NotNull ParserTParser.ImpresionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLinea(@NotNull ParserTParser.LineaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLinea(@NotNull ParserTParser.LineaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDivision(@NotNull ParserTParser.DivisionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDivision(@NotNull ParserTParser.DivisionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDowhile(@NotNull ParserTParser.DowhileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDowhile(@NotNull ParserTParser.DowhileContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSqrt(@NotNull ParserTParser.SqrtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSqrt(@NotNull ParserTParser.SqrtContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSi(@NotNull ParserTParser.SiContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSi(@NotNull ParserTParser.SiContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMenor(@NotNull ParserTParser.MenorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMenor(@NotNull ParserTParser.MenorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(@NotNull ParserTParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(@NotNull ParserTParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAfirmacion(@NotNull ParserTParser.AfirmacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAfirmacion(@NotNull ParserTParser.AfirmacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSin(@NotNull ParserTParser.SinContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSin(@NotNull ParserTParser.SinContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMientras(@NotNull ParserTParser.MientrasContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMientras(@NotNull ParserTParser.MientrasContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFlotante(@NotNull ParserTParser.FlotanteContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFlotante(@NotNull ParserTParser.FlotanteContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLectura(@NotNull ParserTParser.LecturaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLectura(@NotNull ParserTParser.LecturaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMultiplicacion(@NotNull ParserTParser.MultiplicacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMultiplicacion(@NotNull ParserTParser.MultiplicacionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCiclo(@NotNull ParserTParser.CicloContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCiclo(@NotNull ParserTParser.CicloContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMayor(@NotNull ParserTParser.MayorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMayor(@NotNull ParserTParser.MayorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConstan(@NotNull ParserTParser.ConstanContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConstan(@NotNull ParserTParser.ConstanContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDistinto(@NotNull ParserTParser.DistintoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDistinto(@NotNull ParserTParser.DistintoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIgual(@NotNull ParserTParser.IgualContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIgual(@NotNull ParserTParser.IgualContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterResta(@NotNull ParserTParser.RestaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitResta(@NotNull ParserTParser.RestaContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(@NotNull ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(@NotNull ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(@NotNull TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(@NotNull ErrorNode node) { }
+}
---
+++ target/generated-sources/antlr4/ParserTListener.java
@@ -0,0 +1,300 @@
+// Generated from ParserT.g4 by ANTLR 4.4
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ParserTParser}.
+ */
+public interface ParserTListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#suma}.
+	 * @param ctx the parse tree
+	 */
+	void enterSuma(@NotNull ParserTParser.SumaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#suma}.
+	 * @param ctx the parse tree
+	 */
+	void exitSuma(@NotNull ParserTParser.SumaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#asignacion}.
+	 * @param ctx the parse tree
+	 */
+	void enterAsignacion(@NotNull ParserTParser.AsignacionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#asignacion}.
+	 * @param ctx the parse tree
+	 */
+	void exitAsignacion(@NotNull ParserTParser.AsignacionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#receta}.
+	 * @param ctx the parse tree
+	 */
+	void enterReceta(@NotNull ParserTParser.RecetaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#receta}.
+	 * @param ctx the parse tree
+	 */
+	void exitReceta(@NotNull ParserTParser.RecetaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#senlogica}.
+	 * @param ctx the parse tree
+	 */
+	void enterSenlogica(@NotNull ParserTParser.SenlogicaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#senlogica}.
+	 * @param ctx the parse tree
+	 */
+	void exitSenlogica(@NotNull ParserTParser.SenlogicaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#cos}.
+	 * @param ctx the parse tree
+	 */
+	void enterCos(@NotNull ParserTParser.CosContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#cos}.
+	 * @param ctx the parse tree
+	 */
+	void exitCos(@NotNull ParserTParser.CosContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#declavar}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeclavar(@NotNull ParserTParser.DeclavarContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#declavar}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeclavar(@NotNull ParserTParser.DeclavarContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#entero}.
+	 * @param ctx the parse tree
+	 */
+	void enterEntero(@NotNull ParserTParser.EnteroContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#entero}.
+	 * @param ctx the parse tree
+	 */
+	void exitEntero(@NotNull ParserTParser.EnteroContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void enterProgram(@NotNull ParserTParser.ProgramContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void exitProgram(@NotNull ParserTParser.ProgramContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#operacion}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperacion(@NotNull ParserTParser.OperacionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#operacion}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperacion(@NotNull ParserTParser.OperacionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#impresion}.
+	 * @param ctx the parse tree
+	 */
+	void enterImpresion(@NotNull ParserTParser.ImpresionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#impresion}.
+	 * @param ctx the parse tree
+	 */
+	void exitImpresion(@NotNull ParserTParser.ImpresionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#linea}.
+	 * @param ctx the parse tree
+	 */
+	void enterLinea(@NotNull ParserTParser.LineaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#linea}.
+	 * @param ctx the parse tree
+	 */
+	void exitLinea(@NotNull ParserTParser.LineaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#division}.
+	 * @param ctx the parse tree
+	 */
+	void enterDivision(@NotNull ParserTParser.DivisionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#division}.
+	 * @param ctx the parse tree
+	 */
+	void exitDivision(@NotNull ParserTParser.DivisionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#dowhile}.
+	 * @param ctx the parse tree
+	 */
+	void enterDowhile(@NotNull ParserTParser.DowhileContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#dowhile}.
+	 * @param ctx the parse tree
+	 */
+	void exitDowhile(@NotNull ParserTParser.DowhileContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#sqrt}.
+	 * @param ctx the parse tree
+	 */
+	void enterSqrt(@NotNull ParserTParser.SqrtContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#sqrt}.
+	 * @param ctx the parse tree
+	 */
+	void exitSqrt(@NotNull ParserTParser.SqrtContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#si}.
+	 * @param ctx the parse tree
+	 */
+	void enterSi(@NotNull ParserTParser.SiContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#si}.
+	 * @param ctx the parse tree
+	 */
+	void exitSi(@NotNull ParserTParser.SiContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#menor}.
+	 * @param ctx the parse tree
+	 */
+	void enterMenor(@NotNull ParserTParser.MenorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#menor}.
+	 * @param ctx the parse tree
+	 */
+	void exitMenor(@NotNull ParserTParser.MenorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(@NotNull ParserTParser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(@NotNull ParserTParser.StatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#afirmacion}.
+	 * @param ctx the parse tree
+	 */
+	void enterAfirmacion(@NotNull ParserTParser.AfirmacionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#afirmacion}.
+	 * @param ctx the parse tree
+	 */
+	void exitAfirmacion(@NotNull ParserTParser.AfirmacionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#sin}.
+	 * @param ctx the parse tree
+	 */
+	void enterSin(@NotNull ParserTParser.SinContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#sin}.
+	 * @param ctx the parse tree
+	 */
+	void exitSin(@NotNull ParserTParser.SinContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#mientras}.
+	 * @param ctx the parse tree
+	 */
+	void enterMientras(@NotNull ParserTParser.MientrasContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#mientras}.
+	 * @param ctx the parse tree
+	 */
+	void exitMientras(@NotNull ParserTParser.MientrasContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#flotante}.
+	 * @param ctx the parse tree
+	 */
+	void enterFlotante(@NotNull ParserTParser.FlotanteContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#flotante}.
+	 * @param ctx the parse tree
+	 */
+	void exitFlotante(@NotNull ParserTParser.FlotanteContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#lectura}.
+	 * @param ctx the parse tree
+	 */
+	void enterLectura(@NotNull ParserTParser.LecturaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#lectura}.
+	 * @param ctx the parse tree
+	 */
+	void exitLectura(@NotNull ParserTParser.LecturaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#multiplicacion}.
+	 * @param ctx the parse tree
+	 */
+	void enterMultiplicacion(@NotNull ParserTParser.MultiplicacionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#multiplicacion}.
+	 * @param ctx the parse tree
+	 */
+	void exitMultiplicacion(@NotNull ParserTParser.MultiplicacionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#ciclo}.
+	 * @param ctx the parse tree
+	 */
+	void enterCiclo(@NotNull ParserTParser.CicloContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#ciclo}.
+	 * @param ctx the parse tree
+	 */
+	void exitCiclo(@NotNull ParserTParser.CicloContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#mayor}.
+	 * @param ctx the parse tree
+	 */
+	void enterMayor(@NotNull ParserTParser.MayorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#mayor}.
+	 * @param ctx the parse tree
+	 */
+	void exitMayor(@NotNull ParserTParser.MayorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#constan}.
+	 * @param ctx the parse tree
+	 */
+	void enterConstan(@NotNull ParserTParser.ConstanContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#constan}.
+	 * @param ctx the parse tree
+	 */
+	void exitConstan(@NotNull ParserTParser.ConstanContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#distinto}.
+	 * @param ctx the parse tree
+	 */
+	void enterDistinto(@NotNull ParserTParser.DistintoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#distinto}.
+	 * @param ctx the parse tree
+	 */
+	void exitDistinto(@NotNull ParserTParser.DistintoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#igual}.
+	 * @param ctx the parse tree
+	 */
+	void enterIgual(@NotNull ParserTParser.IgualContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#igual}.
+	 * @param ctx the parse tree
+	 */
+	void exitIgual(@NotNull ParserTParser.IgualContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserTParser#resta}.
+	 * @param ctx the parse tree
+	 */
+	void enterResta(@NotNull ParserTParser.RestaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserTParser#resta}.
+	 * @param ctx the parse tree
+	 */
+	void exitResta(@NotNull ParserTParser.RestaContext ctx);
+}
